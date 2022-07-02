d96244a00480fa221cb617bfd4b16a36a6764d8e
Ohleh <oleg.kolischuk@gmail.com>
2022-07-02 23:47:29 +0300

    cmtStyle1

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -18,17 +18,17 @@ const Profile = ({ username, tag, location, avatar, stats }) => {
       </div>
 
       <ul className={css.stats}>
-        <li>
-          <span className="label">Followers</span>
-          <span className="quantity">{stats.followers}</span>
+        <li className={css.statsList}>
+          <span className={css.label}>Followers </span>
+          <span className={css.quantity}>{stats.followers}</span>
         </li>
-        <li>
-          <span className="label">Views</span>
-          <span className="quantity">{stats.views}</span>
+        <li className={css.statsList}>
+          <span className={css.label}>Views </span>
+          <span className={css.quantity}>{stats.views}</span>
         </li>
-        <li>
-          <span className="label">Likes</span>
-          <span className="quantity">{stats.likes}</span>
+        <li className={css.statsList}>
+          <span className={css.label}>Likes </span>
+          <span className={css.quantity}>{stats.likes}</span>
         </li>
       </ul>
     </div>
