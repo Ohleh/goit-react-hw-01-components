6bafb10494db6a6533d1bfa9f0acd31d85522c78
Ohleh <oleg.kolischuk@gmail.com>
2022-07-03 01:01:44 +0300

    cmtstl

--- src/components/Statistics/index.jsx
+++ src/components/Statistics/index.jsx
@@ -5,13 +5,13 @@ const Statistics = ({ title, stats }) => {
   return (
     <section className={css.statistics}>
       {/* check for title, render H2 */}
-      {title ? <h2 className="title">{title}</h2> : ''}
+      {title ? <h2 className={css.title}>{title}</h2> : ''}
 
       <ul className={css.statList}>
         {stats.map(el => (
-          <li className="item" key={el.id}>
-            <span className="label">{el.label} </span>
-            <span className="percentage">{el.percentage}%</span>
+          <li className={css.item} key={el.id}>
+            <span className={css.label}>{el.label} </span>
+            <span className={css.percentage}>{el.percentage}%</span>
           </li>
         ))}
       </ul>
