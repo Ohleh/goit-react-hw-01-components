68b1140fb13fc28f6b80f1fb675126df8d931a2b
Ohleh <oleg.kolischuk@gmail.com>
2022-07-02 20:16:28 +0300

    cmtt

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,33 +1,33 @@
 import propTypes from 'prop-types';
-import cssTitle from '../Profile/Profile.module.css';
+import css from '../Profile/Profile.module.css';
 
 const Profile = ({ username, tag, location, avatar, stats }) => {
   return (
-    <div className={cssTitle.profile}>
+    <div className={css.profile}>
       <div className="description">
         <img
-          key={username}
+          // key={username}
           src={avatar}
           alt="User avatar"
-          className="avatar"
+          className={css.avatar}
           width="200"
         />
-        <p className="name">{username}</p>
+        <p className={css.name}>{username}</p>
         <p className="tag">@{tag}</p>
         <p className="location">{location}</p>
       </div>
 
-      <ul className="stats">
+      <ul className={css.stats}>
         <li>
-          <span className="label">Followers - </span>
+          <span className="label">Followers</span>
           <span className="quantity">{stats.followers}</span>
         </li>
         <li>
-          <span className="label">Views - </span>
+          <span className="label">Views</span>
           <span className="quantity">{stats.views}</span>
         </li>
         <li>
-          <span className="label">Likes - </span>
+          <span className="label">Likes</span>
           <span className="quantity">{stats.likes}</span>
         </li>
       </ul>
